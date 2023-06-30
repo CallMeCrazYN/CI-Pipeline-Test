d88a8b2af220f2ea1774a1d69c7d5e43e2026e0f
Niggo <nicolaskautt01@gmail.com>
2023-06-30 17:59:26 +0200

    change tests for prime

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class PrimeCheckTest {
+  //In Zusamenarbeit mit Lukas Eichel, Yasmin Seefried, Nicolas Kautt
   // --------------------------------------------------------------- //
   @Test
   public void TestNegativeNumbers() {
@@ -22,14 +23,18 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primes = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541};
+    for (int prime : primes) {
+      assertEquals(prime + " should be a prime number", true, PrimeCheck.isPrime(prime));
+    }
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] notPrimes = {4,6,8,10,12,14,16,18,20,420, 69, 312, 508};
+    for (int nprime : notPrimes) {
+      assertEquals(nprime + " shouldnt be a prime number", false, PrimeCheck.isPrime(nprime));
+    }
   }
 }
